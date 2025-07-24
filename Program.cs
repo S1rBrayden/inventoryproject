4fadd57da1547bded512eb3fed902e5d1752f99d
Brayden <156861320+S1rBrayden@users.noreply.github.com>
2025-07-24 13:06:27 -0600

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IInventoryService, InventoryService>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Middleware
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
